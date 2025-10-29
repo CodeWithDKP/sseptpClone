c6290489ce63663bc2885e85db1fe82b31e27991
Dinesh kumar <dineshkumarptech@gmail.com>
2025-10-29 16:22:27 +0530

    added spinner for loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
-
-
-import Home from './pages/Home.jsx';
+import { Suspense, lazy } from 'react';
 import './App.css';
+
+const Home = lazy(() => import('./pages/Home.jsx'));
+
 function App() {
   return (
-    <>
-      <Home/>
-    </>
+    <Suspense
+      fallback={
+        <div className="spinner-container">
+          <div className="spinner"></div>
+          <p>Loading...</p>
+        </div>
+      }
+    >
+      <Home />
+    </Suspense>
   );
 }
 
